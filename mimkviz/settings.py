b500c1d198ae4fe34705705fc522ea12c8fe8781
Milos <milos3design@hotmail.com>
2024-04-11 20:47:35 +0200

    allowed hosts link added

--- mimkviz/settings.py
+++ mimkviz/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "localhost:5173"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "localhost:5173", "mimkvizapi.milos3design.com"]
 
 
 # Application definition
